9396ff12362071b6cbb005c829df04f26e034bc8
Onyinyechi14 <giftjulius070@gmail.com>
2024-09-22 15:27:54 +0100

    something new

--- src/Component/Footer/footer.jsx
+++ src/Component/Footer/footer.jsx
@@ -84,7 +84,7 @@ function Footer() {
         </div>
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
         >
           Submit
         </button>
